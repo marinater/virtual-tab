f3b84f257b1388dd8c0ce65ab70267e0c3525ff1
Samarth Patel <samarthxpatel@gmail.com>
2021-04-18 23:38:32 -0400

    Improve pen tracking algo

--- core/process.py
+++ core/process.py
@@ -10,16 +10,25 @@ def process_frame(image_in):
     image_thresh = cv2.inRange(image_hsv, params.thresh_pen_min, params.thresh_pen_max)
     image_thresh = cv2.morphologyEx(image_thresh, cv2.MORPH_OPEN, np.ones((5,5), np.uint8))
 
-    # image_thresh_overlayed = image_in.copy()
-    # image_thresh_overlayed[image_thresh == 255, :] = (0, 0, 255)
+    image_thresh_overlayed = image_in.copy()
+    image_thresh_overlayed[image_thresh == 255, :] = (0, 0, 255)
     # io.show_frame('thresholded', image_thresh_overlayed)
 
-    contours, hierarchy = cv2.findContours(image_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    if contours:
-        largest_contour = max([(cv2.contourArea(contour), contour) for contour in contours], key= lambda x: x[0])
-        print(largest_contour[0])
+    count, labeled, stats, centroids = cv2.connectedComponentsWithStats(image_thresh, 4, cv2.CV_32S)
+    if count > 1:
+        largest_label = np.argmax(stats[1:, cv2.CC_STAT_AREA]) + 1
+        print(largest_label, stats[largest_label, cv2.CC_STAT_AREA])
 
-    return image_thresh
+    # if contours:
+    #     largest_contour_idx, largest_contour_area = max([
+    #         (i, cv2.contourArea(contour))
+    #         for i, contour
+    #         in enumerate(contours)
+    #     ], key= lambda x: x[1])
+
+    #     cv2.drawContours(image_thresh_overlayed, contours, largest_contour_idx, (0, 255, 0), -1)
+
+    return image_thresh_overlayed
 
 def run():
     while True:
