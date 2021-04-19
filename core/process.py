772abd0944469f08fa266b24cc897753dc9733f7
Peter Evans <peter-evans@users.noreply.github.com>
2021-04-19 22:09:53 +0000

    Automated changes

--- core/process.py
+++ core/process.py
@@ -5,33 +5,49 @@ import cv2
 
 io = ImageIO()
 
+
 def round_tuple(x):
     return int(round(x[0])), int(round(x[1]))
+
 
 def process_frame(tracker):
     image_in = io.get_frame()
 
     image_hsv = cv2.cvtColor(image_in, cv2.COLOR_BGR2HSV)
     image_thresh = cv2.inRange(image_hsv, params.thresh_pen_min, params.thresh_pen_max)
-    image_thresh = cv2.morphologyEx(image_thresh, cv2.MORPH_OPEN, np.ones((5,5), np.uint8))
+    image_thresh = cv2.morphologyEx(
+        image_thresh, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8)
+    )
 
     image_thresh_overlayed = image_in.copy()
     image_thresh_overlayed[image_thresh == 255, :] = (0, 0, 255)
     # io.show_frame('thresholded', image_thresh_overlayed)
 
-    count, labeled, stats, centroids = cv2.connectedComponentsWithStats(image_thresh, 4, cv2.CV_32S)
+    count, labeled, stats, centroids = cv2.connectedComponentsWithStats(
+        image_thresh, 4, cv2.CV_32S
+    )
     if count > 1:
         largest_label = np.argmax(stats[1:, cv2.CC_STAT_AREA]) + 1
         pen_center = centroids[largest_label]
 
         image_thresh_overlayed[labeled == largest_label, :] = (255, 100, 0)
-        cv2.drawMarker(image_thresh_overlayed, round_tuple(pen_center), (100, 255, 0), cv2.MARKER_CROSS, 10, 2)
+        cv2.drawMarker(
+            image_thresh_overlayed,
+            round_tuple(pen_center),
+            (100, 255, 0),
+            cv2.MARKER_CROSS,
+            10,
+            2,
+        )
         tracker.setPosition(pen_center)
 
     if not tracker.tabletPoly is None:
-        cv2.polylines(image_thresh_overlayed, tracker.tabletPoly, True, (255, 255, 255), 2)
+        cv2.polylines(
+            image_thresh_overlayed, tracker.tabletPoly, True, (255, 255, 255), 2
+        )
 
-    return io.show_frame('output', image_thresh_overlayed, wait=True, tracker=tracker)
+    return io.show_frame("output", image_thresh_overlayed, wait=True, tracker=tracker)
+
 
 def tracking_loop(tracker):
     while process_frame(tracker):
--- core/tracking.py
+++ core/tracking.py
@@ -3,13 +3,17 @@ from .process import round_tuple, tracking_loop
 import numpy as np
 import cv2
 import pyautogui
+
 pyautogui.PAUSE = 0
 
 w_max = 1920
 h_max = 1080
+
+
 def cvPointToScreenPoint(cv_point):
     row, col = map(lambda x: int(max(0, round(x))), cv_point)
     return min(w_max - 1, col), min(h_max - 1, row)
+
 
 class Tracking:
     def __init__(self):
@@ -30,7 +34,9 @@ class Tracking:
         if len(self._cornersRaw) == 4:
             self.corners = np.array(self._cornersRaw, dtype=np.float32)
             self._cornersRaw.append(self._cornersRaw[0])
-            self.tabletPoly = [np.array(self._cornersRaw, dtype=np.int32).reshape((-1,1,2))]
+            self.tabletPoly = [
+                np.array(self._cornersRaw, dtype=np.int32).reshape((-1, 1, 2))
+            ]
             self._cornersRaw = []
             self.setTransform()
 
@@ -39,12 +45,15 @@ class Tracking:
             return
 
         src = self.corners
-        dst = np.array([
-            [0, 0],
-            [h_max - 1, 0],
-            [h_max - 1, w_max - 1],
-            [0, w_max - 1],
-        ], dtype=np.float32)
+        dst = np.array(
+            [
+                [0, 0],
+                [h_max - 1, 0],
+                [h_max - 1, w_max - 1],
+                [0, w_max - 1],
+            ],
+            dtype=np.float32,
+        )
 
         self.transform = cv2.getPerspectiveTransform(src, dst)
         pyautogui.sleep(0.5)
