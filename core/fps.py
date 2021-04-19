f1a45df251df4f96ed44c0e3cadf1485b1af6d0a
Samarth Patel <samarthxpatel@gmail.com>
2021-04-19 22:33:48 +0000

    Apply Black formatting to Python files

--- core/fps.py
+++ core/fps.py
@@ -3,20 +3,20 @@ from collections import deque
 
 max_queue_length = 30
 
+
 class FPS:
-	def __init__(self):
-		self._frame_start = None
-		self.prev_rates = deque()
+    def __init__(self):
+        self._frame_start = None
+        self.prev_rates = deque()
 
-	def start_frame(self):
-		self._frame_start = timer()
-		if len(self.prev_rates) > max_queue_length:
-			self.prev_rates.popleft()
+    def start_frame(self):
+        self._frame_start = timer()
+        if len(self.prev_rates) > max_queue_length:
+            self.prev_rates.popleft()
 
+    def stop_frame(self):
+        elapsed = timer() - self._frame_start
+        self._frame_start = None
+        self.prev_rates.append(1 / elapsed)
 
-	def stop_frame(self):
-		elapsed = timer() - self._frame_start
-		self._frame_start = None
-		self.prev_rates.append(1 / elapsed)
-
-		return sum(self.prev_rates) / len(self.prev_rates)
+        return sum(self.prev_rates) / len(self.prev_rates)
