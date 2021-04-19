4bd968ef2d8c88a7500a5b7cac43e28d9742538c
Samarth Patel <samarthxpatel@gmail.com>
2021-04-19 03:21:04 -0400

    Get screen coordinates by performing perspective transform

--- core/process.py
+++ core/process.py
@@ -5,6 +5,9 @@ import cv2
 
 io = ImageIO()
 
+def round_tuple(x):
+    return int(round(x[0])), int(round(x[1]))
+
 def process_frame(tracker):
     image_in = io.get_frame()
 
@@ -19,20 +22,16 @@ def process_frame(tracker):
     count, labeled, stats, centroids = cv2.connectedComponentsWithStats(image_thresh, 4, cv2.CV_32S)
     if count > 1:
         largest_label = np.argmax(stats[1:, cv2.CC_STAT_AREA]) + 1
-        pen_center = (
-            int(round(centroids[largest_label][0])),
-            int(round(centroids[largest_label][1]))
-        )
+        pen_center = centroids[largest_label]
 
         image_thresh_overlayed[labeled == largest_label, :] = (255, 100, 0)
-        cv2.drawMarker(image_thresh_overlayed, pen_center, (100, 255, 0), cv2.MARKER_CROSS, 10, 2)
-        tracker.position = pen_center
+        cv2.drawMarker(image_thresh_overlayed, round_tuple(pen_center), (100, 255, 0), cv2.MARKER_CROSS, 10, 2)
+        tracker.setPosition(pen_center)
 
-    if tracker.corners:
+    if not tracker.tabletPoly is None:
         cv2.polylines(image_thresh_overlayed, tracker.tabletPoly, True, (255, 255, 255), 2)
 
     return io.show_frame('output', image_thresh_overlayed, wait=True, tracker=tracker)
-
 
 def tracking_loop(tracker):
     while process_frame(tracker):
